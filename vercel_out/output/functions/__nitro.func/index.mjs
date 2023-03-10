08a32e1839a71dbc650bc3b0117a54b0c0e94fb5
Pooya Parsa <pooya@pi0.io>
2023-03-10 14:50:26 +0100

    update

--- vercel_out/output/functions/__nitro.func/index.mjs
+++ vercel_out/output/functions/__nitro.func/index.mjs
@@ -2,7 +2,7 @@ globalThis.test = "123";
 
 export default async function handleEvent(request, event) {
   // This works
-  return new Response(`Test value: ${globalThis.test}`);
+  // return new Response(`Test value: ${globalThis.test}`);
   // This won't
-  // return new Response(`Test value: ${test}`);
+  return new Response(`Test value: ${test}`);
 }
