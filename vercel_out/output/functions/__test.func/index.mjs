bbc154e06a070ddbd8a4e7d1fc2ca2a3b3fff912
Pooya Parsa <pooya@pi0.io>
2023-12-21 21:26:57 +0100

    update

--- vercel_out/output/functions/__test.func/index.mjs
+++ vercel_out/output/functions/__test.func/index.mjs
@@ -1,7 +1,7 @@
-// import wasmModule from './wasm/sum.wasm?module';
+import wasmModule from './wasm/sum.wasm';
 
 export default async function handler() {
-  const wasmModule = await import('./wasm/sum.wasm').then(r => r.default)
+  // const wasmModule = await import('./wasm/sum.wasm').then(r => r.default)
   const { exports } = await WebAssembly.instantiate(wasmModule)
-  return new Response(`2+2=${exports.sum(2, 2)}`);
+  return new Response(`1+2=${exports.sum(1, 2)}`);
 }
